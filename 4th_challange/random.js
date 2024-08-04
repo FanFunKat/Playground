870adf9655b2611e5d232a4b556133c0c32dd643
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-04 17:01:02 +0200

    random number - function

--- 4th_challange/random.js
+++ 4th_challange/random.js
@@ -1,33 +1,32 @@
 // Collect inputs from a user
-const userInputLow = prompt("Please provide your lowest number");
-const userInputHigh = prompt("Please provide your highest number")
+// const userInputLow = prompt("Please provide your lowest number");
+// const userInputHigh = prompt("Please provide your highest number")
 
 // Convert the inputs to a number
-const userNumberLow = parseInt(userInputLow);
-const userNumberHigh = parseInt(userInputHigh);
-
-
+// const userNumberLow = parseInt(userInputLow);
+// const userNumberHigh = parseInt(userInputHigh);
 const mainElement = document.querySelector('main');
+const message = "coś";
 
-// Use Math.random() and the user's number to generate a random number
+function getRandom(userNumberLow, userNumberHigh) {
+  let message = ``;
 
-if (userNumberLow && userInputHigh && userNumberLow < userNumberHigh) {
-  const randomNumber = Math.floor(Math.random() * (userNumberHigh - userNumberLow + 1)) + userNumberLow;
-  const message = `<p>${randomNumber} is random number between ${userNumberLow} and ${userNumberHigh}</p>`;
-  mainElement.innerHTML = message;
-} else if (userNumberLow && userNumberHigh && userNumberLow > userNumberHigh) {
-  const message = `<p>${userNumberHigh} is lower than ${userNumberLow}, please try again</p>`;
-  mainElement.innerHTML = message;
-} else if (userNumberLow && userNumberHigh && userNumberLow === userNumberHigh) {
-  const message = `<p>${userNumberHigh} is equal to ${userNumberLow}, please try again</p>`;
-  mainElement.innerHTML = message;
-} else if (!userNumberLow && userNumberHigh) {
-  const errorMessage = `<p>'${userInputLow}' is not a number</p>`;
-  mainElement.innerHTML = errorMessage;
-} else if (userNumberLow && !userNumberHigh) {
-  const errorMessage = `<p>'${userInputHigh}' is not a number</p>`;
-  mainElement.innerHTML = errorMessage;
-} else {
-  const errorMessage = `<p>'${userInputLow}' and '${userInputHigh}' are not numbers</p>`;
-  mainElement.innerHTML = errorMessage;
+  if (userNumberLow && userNumberHigh && userNumberLow < userNumberHigh) {
+    const randomNumber = Math.floor(Math.random() * (userNumberHigh - userNumberLow + 1)) + userNumberLow;
+    message = `<p>${randomNumber} is random number between ${userNumberLow} and ${userNumberHigh}</p>`;
+  } else if (userNumberLow && userNumberHigh && userNumberLow > userNumberHigh) {
+    message = `<p>${userNumberHigh} is lower than ${userNumberLow}, please try again</p>`;
+  } else if (userNumberLow && userNumberHigh && userNumberLow === userNumberHigh) {
+    message = `<p>${userNumberHigh} is equal to ${userNumberLow}, please try again</p>`;
+  } else {
+    message = `<p>Error! Both inputs must be numbers.</p>`;
+  }
+
+  return message
 }
+
+// Display the message for users in prompt, change string to number!
+const minNumb = parseInt(prompt("Please provide your lowest number"));
+const maxNumb = parseInt(prompt("Please provide your highest number"));
+
+mainElement.innerHTML = getRandom(minNumb, maxNumb);
