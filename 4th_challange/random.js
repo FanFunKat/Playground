94a2aaa7ba0c88e39815ef7258e2e64b88169e69
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-04 12:29:13 +0200

    conditional expression added

--- 4th_challange/random.js
+++ 4th_challange/random.js
@@ -5,10 +5,16 @@ const userInput = prompt("Pleas enter your number");
 const userNumber = parseInt(userInput);
 
 // Use Math.random() and the user's number to generate a random number
-const randomNumber = Math.floor(Math.random() * userNumber) + 1;
-
-// Create a message displaying the random number
 const mainElement = document.querySelector('main');
-const message = `<p>${randomNumber} is random number between 1 and ${userNumber}</p>`
-mainElement.innerHTML = message
 
+if (userNumber) {
+  const randomNumber = Math.floor(Math.random() * userNumber) + 1;
+
+  // Create a message displaying the random number
+  const message = `<p>${randomNumber} is random number between 1 and ${userNumber}</p>`;
+  mainElement.innerHTML = message;
+
+} else {
+  const errorMessage = `<p>'${userInput}' is not a number</p>`;
+  mainElement.innerHTML = errorMessage;
+}
