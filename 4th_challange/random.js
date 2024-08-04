8d3e0b1339b5ff942cbbe1a8ce21a5a86ce44076
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-04 12:56:22 +0200

    random number between 2 numbers + messages

--- 4th_challange/random.js
+++ 4th_challange/random.js
@@ -1,20 +1,33 @@
-// Collect input from a user
-const userInput = prompt("Pleas enter your number");
+// Collect inputs from a user
+const userInput = prompt("Please enter your number");
+const userSecondInput = prompt("Please enter a higher number than the previous one")
 
-// Convert the input to a number
+// Convert the inputs to a number
 const userNumber = parseInt(userInput);
+const userHigherNumber = parseInt(userSecondInput);
 
-// Use Math.random() and the user's number to generate a random number
+
 const mainElement = document.querySelector('main');
 
-if (userNumber) {
-  const randomNumber = Math.floor(Math.random() * userNumber) + 1;
+// Use Math.random() and the user's number to generate a random number
 
-  // Create a message displaying the random number
-  const message = `<p>${randomNumber} is random number between 1 and ${userNumber}</p>`;
+if (userNumber && userHigherNumber && userNumber < userHigherNumber) {
+  const randomNumber = Math.floor(Math.random() * (userHigherNumber - userNumber + 1)) + userNumber;
+  const message = `<p>${randomNumber} is random number between ${userNumber} and ${userHigherNumber}</p>`;
   mainElement.innerHTML = message;
-
-} else {
+} else if (userNumber && userHigherNumber && userNumber > userHigherNumber) {
+  const message = `<p>${userHigherNumber} is lower than ${userNumber}, please try again</p>`;
+  mainElement.innerHTML = message;
+} else if (userNumber && userHigherNumber && userNumber === userHigherNumber) {
+  const message = `<p>${userHigherNumber} is equal to ${userNumber}, please try again</p>`;
+  mainElement.innerHTML = message;
+} else if (!userNumber && userHigherNumber) {
   const errorMessage = `<p>'${userInput}' is not a number</p>`;
+  mainElement.innerHTML = errorMessage;
+} else if (userNumber && !userHigherNumber) {
+  const errorMessage = `<p>'${userSecondInput}' is not a number</p>`;
+  mainElement.innerHTML = errorMessage;
+} else {
+  const errorMessage = `<p>'${userInput}' and '${userSecondInput}' are not numbers</p>`;
   mainElement.innerHTML = errorMessage;
 }
