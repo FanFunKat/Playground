1795080dcb8954408b33cd447df5eec60c6a0a09
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-04 21:58:16 +0200

    Refactored - RGB color code

--- 5th_challenge/refactor.js
+++ 5th_challenge/refactor.js
@@ -1,14 +1,12 @@
 let html = '';
-let red;
-let green;
-let blue;
 let randomRGB;
 
-for (let i = 1; i <= 10; i++) {
-  red = Math.floor(Math.random() * 256);
-  green = Math.floor(Math.random() * 256);
-  blue = Math.floor(Math.random() * 256);
-  randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+function randomRGBNumber() {
+  return Math.floor(Math.random() * 256);
+}
+
+for (let i = 1; i <= 100; i++) {
+  randomRGB = `rgb( ${randomRGBNumber()}, ${randomRGBNumber()}, ${randomRGBNumber()} )`;
   html += `<div style="background-color: ${randomRGB}">${i}</div>`;
 }
 
