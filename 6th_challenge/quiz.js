0a074e6d15d3e6408fe024d12716bebac1c30ad8
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-05 13:31:21 +0200

    list of questions and answers

--- 6th_challenge/quiz.js
+++ 6th_challenge/quiz.js
@@ -1,10 +1,16 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
 
+const quiz = [
+  ["What has keys but can't open locks?", "A piano"],
+  ["I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?", "An echo"],
+  ["I'm tall when I'm young, and I'm short when I'm old. What am I?", "A candle"]
+];
+
 
 // 2. Store the number of questions answered correctly
 
 
-/* 
+/*
   3. Use a loop to cycle through each question
       - Present each question to the user
       - Compare the user's response to answer in the array
