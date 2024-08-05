d5a550a42ac93cdd1058bade8ba64a453f465f3d
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-05 17:57:25 +0200

    7th challenge "pets"

---
+++ 7th_challenge/js/pets.js
@@ -0,0 +1,50 @@
+/*
+  Create an array of 'pet' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+
+const pets = [
+  {
+    name: 'Rex',
+    type: 'Dog',
+    bread: 'aussie',
+    age: 2,
+    photo: '../img/aussie.jpg',
+  },
+  {
+    name: 'Zoe',
+    type: 'Dog',
+    bread: 'dach shund',
+    age: 4,
+    photo: '../img/dauchshund.jpg',
+  },
+  {
+    name: 'Pinmek',
+    type: 'Dog',
+    bread: 'golden',
+    age: 5,
+    photo: '../img/golden.jpg',
+  },
+  {
+    name: 'Mruczek',
+    type: 'Cat',
+    bread: 'persian',
+    age: 3,
+    photo: '../img/persian.jpg',
+  },
+  {
+    name: 'Bobek',
+    type: 'Dog',
+    bread: 'pug',
+    age: 2,
+    photo: '../img/pug.jpg',
+  },
+  {
+    name: 'Kot',
+    type: 'Cat',
+    bread: 'tabby',
+    age: 5,
+    photo: '../img/tabby.jpg',
+  },
+]
