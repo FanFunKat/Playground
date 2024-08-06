40f21f9d810f1593f7c2bd30d035dd8999331e51
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-06 18:19:21 +0200

    select by ID

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -1,5 +1,12 @@
-const body = document.body
+// const body = document.body
 
-body.addEventListener('click', () => {
-  body.innerHTML = '<h1>Hello World!</h1>';
+// body.addEventListener('click', () => {
+//   body.innerHTML = '<h1>Hello World!</h1>';
+// });
+
+const headline = document.getElementById('headline');
+const btnMain = document.getElementById('btn-main');
+
+btnMain.addEventListener('click', () => {
+  headline.style.border = '1px solid red';
 });
