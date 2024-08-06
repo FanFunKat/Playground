32d54f92c46aae51c7d975187d886abc6cbe4470
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-06 21:33:04 +0200

    changed selection method to query... and changed for in to for of in lne 21

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -4,22 +4,25 @@
 //   body.innerHTML = '<h1>Hello World!</h1>';
 // });
 
-const headline = document.getElementById('headline');
-const btnMain = document.getElementById('btn-main');
+// const btnMain = document.getElementById('btn-main');
+const btnMain = document.querySelector('.btn-main');
 
-const items = document.getElementsByTagName('li');
+// const items = document.getElementsByTagName('li');
+const items = document.querySelectorAll('li');
 
-const highlights = document.getElementsByClassName('highlight');
+
+// const highlights = document.getElementsByClassName('highlight');
+const highlights = document.querySelectorAll('.highlight');
 
 for (const highlight of highlights) {
   highlight.style.color = 'orchid';
 }
 
-for (let item in items) {
-  items[item].style.backgroundColor = 'skyblue';
+for (const item of items) {
+  item.style.backgroundColor = 'lightblue';
 }
 
 btnMain.addEventListener('click', () => {
-  headline.style.border = '1px solid red';
+  btnMain.style.border = '1px solid red';
 });
 
