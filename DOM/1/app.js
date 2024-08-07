62b647079fea4bf422c8daf50088e92008dfd804
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-07 12:59:58 +0200

    text content

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -1,28 +1,10 @@
-// const body = document.body
-
-// body.addEventListener('click', () => {
-//   body.innerHTML = '<h1>Hello World!</h1>';
-// });
-
-// const btnMain = document.getElementById('btn-main'); 
 const btnMain = document.querySelector('.btn-main');
 
-// const items = document.getElementsByTagName('li'); //HTMLCollection
-const items = document.querySelectorAll('li'); //NodeList
-
-
-// const highlights = document.getElementsByClassName('highlight'); //HTMLCollection
-const highlights = document.querySelectorAll('.highlight'); //NodeList
-
-for (const highlight of highlights) {
-  highlight.style.color = 'orchid';
-}
-
-for (const item of items) {
-  item.style.backgroundColor = 'lightblue';
-}
 
 btnMain.addEventListener('click', () => {
-  btnMain.style.border = '1px solid red';
-});
+  const headline = document.getElementById('headline');
+  const input = document.querySelector('.input-main');
 
+  headline.textContent = input.value;
+  input.value = '';
+})
