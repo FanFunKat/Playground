e74aa9d9a56d1083bff333051fab5727a0adda3b
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-07 13:22:05 +0200

    adding class

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -5,6 +5,7 @@ btnMain.addEventListener('click', () => {
   const headline = document.getElementById('headline');
   const input = document.querySelector('.input-main');
 
+  headline.className += 'grow'
   headline.textContent = input.value;
   input.value = '';
 })
