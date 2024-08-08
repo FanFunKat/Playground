467427b9e793810939e482d8e31bbf24a0318f5c
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-08 14:37:54 +0200

    add mouseover on first item

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -1,6 +1,12 @@
 const btnCreate = document.querySelector('.btn-main');
 const btnToggle = document.querySelector('.btn-toggle');
 const btnRemove = document.querySelector('.btn-remove');
+const listContainer = document.querySelector('.list-container');
+const firstItem = document.getElementsByTagName('li')[0];
+
+firstItem.addEventListener('mouseover', () => {
+  firstItem.textContent = firstItem.textContent.toUpperCase();
+});
 
 btnCreate.addEventListener('click', () => {
   const input = document.querySelector('.input-main');
@@ -14,7 +20,6 @@ btnCreate.addEventListener('click', () => {
 })
 
 btnToggle.addEventListener('click', () => {
-  const listContainer = document.querySelector('.list-container');
   if (listContainer.style.display === 'none') {
     listContainer.removeAttribute('style');
     btnToggle.textContent = 'Hide List';
