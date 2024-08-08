83ddc3c7595d3a65652149a06a2d57acecb66977
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-08 15:02:34 +0200

    event.target

--- DOM/1/app.js
+++ DOM/1/app.js
@@ -4,11 +4,24 @@ const btnRemove = document.querySelector('.btn-remove');
 const listContainer = document.querySelector('.list-container');
 const listItems = document.getElementsByTagName('li');
 
-for (let i = 0; i < listItems.length; i++) { //but it doesn't work for the new items
-  listItems[i].addEventListener('mouseover', () => {
-    listItems[i].textContent = listItems[i].textContent.toUpperCase();
-  });
-}
+//for (let i = 0; i < listItems.length; i++) { //but it doesn't work for the new items
+//   listItems[i].addEventListener('mouseover', () => {
+//     listItems[i].textContent = listItems[i].textContent.toUpperCase();
+//   });
+// }
+
+// for (let i = 0; i < listItems.length; i++) {
+//   listItems[i].addEventListener('mouseover', () => {
+//     listItems[i].classList.add('list-item-hover');
+//   });
+//   listItems[i].addEventListener('mouseout', () => {
+//     listItems[i].classList.remove('list-item-hover');
+//   });
+// }
+
+document.addEventListener('click', (event) => {
+  console.log(event.target);
+});
 
 btnCreate.addEventListener('click', () => {
   const input = document.querySelector('.input-main');
