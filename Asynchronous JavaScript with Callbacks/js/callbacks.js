f5e21e63aabcca08ed77127f5a4ea92548f4e010
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-12 22:27:44 +0200

    added getProfiles function

--- Asynchronous JavaScript with Callbacks/js/callbacks.js
+++ Asynchronous JavaScript with Callbacks/js/callbacks.js
@@ -16,6 +16,12 @@ function getJSON(url, callback) { //higher order function
   xhr.send();
 }
 
+function getProfiles(json) {
+  json.people.map(person => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  });
+}
+
 // Generate the markup for each profile
 function generateHTML(data) {
   const section = document.createElement('section');
@@ -39,10 +45,6 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (e) => {
-  getJSON(astrosUrl, (json) => {
-    json.people.map(person => {
-      getJSON(wikiUrl + person.name, generateHTML);
-    })
-  });
+  getJSON(astrosUrl, getProfiles);
   e.target.remove();
 });
