b9b2484dfa0580393d58b2e7098894008f8134d8
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-13 22:14:19 +0200

    Fetch... but there is an error. For now, I only know that not all names from Astros match those on Wikipedia

--- Asynchronous JavaScript with Callbacks/js/promises.js
+++ Asynchronous JavaScript with Callbacks/js/promises.js
@@ -3,33 +3,31 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        resolve(data);
-      } else {
-        reject(Error(xhr.statusText));
-      }
-    };
-    xhr.onerror = () => reject(Error("Network Error"));
-    xhr.send();
-  });
-}
+// function getJSON(url) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.onload = () => {
+//       if (xhr.status === 200) {
+//         let data = JSON.parse(xhr.responseText);
+//         resolve(data);
+//       } else {
+//         reject(Error(xhr.statusText));
+//       }
+//     };
+//     xhr.onerror = () => reject(Error("Network Error"));
+//     xhr.send();
+//   });
+// }
 
 function getProfiles(json) {
   const profiles = json.people.map(person => {
-    return getJSON(wikiUrl + person.name);
+    return fetch(wikiUrl + person.name).then(response => response.json());
   });
-  // console.log(Promise.all(profiles));
+  console.log(Promise.all(profiles));
   return Promise.all(profiles);
-
 }
 
-// Generate the markup for each profile
 function generateHTML(data) {
   data.forEach(person => {
     const section = document.createElement('section');
@@ -42,6 +40,13 @@ function generateHTML(data) {
         <p>${person.description}</p>
         <p>${person.extract}</p>
       `;
+    } else if (person.type === 'standard' && person.name + '(taikonaut)') {
+      section.innerHTML = `
+        <img src=${person.thumbnail.source}>
+        <h2>${person.title}</h2>
+        <p>${person.description}</p>
+        <p>${person.extract}</p>
+        `;
     } else {
       section.innerHTML = `
         <img src="img/profile.jpg" alt="ocean clouds seen from space">
@@ -55,11 +60,12 @@ function generateHTML(data) {
 
 btn.addEventListener('click', (event) => {
   event.target.textContent = 'Loading...';
-  getJSON(astrosUrl)
+  fetch(astrosUrl)
+    .then(response => response.json())
     .then(getProfiles)
     .then(generateHTML)
     .catch(err => {
-      peopleList.innerHTML = '<h3>Error</h3><p>Something went wrong. Unable to load profiles</p>';
+      // peopleList.innerHTML = '<h3>Error</h3><p>Something went wrong. Unable to load profiles</p>';
       console.log(err)
     })
     .finally(() => event.target.remove())
