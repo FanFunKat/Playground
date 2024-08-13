6adaf5a4011c2495592ac174d4f00165e2b7eb07
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-13 14:13:38 +0200

    reject promise - catch

--- Asynchronous JavaScript with Callbacks/js/promises-breakfast.js
+++ Asynchronous JavaScript with Callbacks/js/promises-breakfast.js
@@ -1,9 +1,13 @@
 const breakfastPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve('Pancakes');
+    reject('No pancakes :(');
   }, 3000);
 });
 
-breakfastPromise.then(breakfast => {
-  console.log(`Yay! I have ${breakfast} for breakfast!`);
-});
+breakfastPromise
+  .then(breakfast => {
+    console.log(`Yay! I have ${breakfast} for breakfast!`);
+  })
+  .catch(error => {
+    console.log(`Oh no! ${error}`);
+  })
