9f23ffec9c8ea53e3ba818d3edf9c475c9efd142
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-13 14:19:45 +0200

    added if statement

--- Asynchronous JavaScript with Callbacks/js/promises-breakfast.js
+++ Asynchronous JavaScript with Callbacks/js/promises-breakfast.js
@@ -1,13 +1,15 @@
+const order = false;
+
 const breakfastPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject('No pancakes :(');
+    if (order) {
+      resolve('Pancakes');
+    } else {
+      reject(Error('No pancakes :('));
+    }
   }, 3000);
 });
 
 breakfastPromise
-  .then(breakfast => {
-    console.log(`Yay! I have ${breakfast} for breakfast!`);
-  })
-  .catch(error => {
-    console.log(`Oh no! ${error}`);
-  })
+  .then(breakfast => console.log(`Yay! I have ${breakfast} for breakfast!`))
+  .catch(error => console.log(`Oh no! ${error}`))
