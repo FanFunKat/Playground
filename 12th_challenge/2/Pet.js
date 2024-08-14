42f8958600989ccf9031dd4ba991c94b2b28668d
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-14 18:52:21 +0200

    constructor method

---
+++ 12th_challenge/2/Pet.js
@@ -0,0 +1,8 @@
+class Pet {
+  constructor(name, animal, age, breed) { //constructor method - special method for creating and initializing an object created with a class.
+    this.name = name;
+    this.animal = animal;
+    this.age = age;
+    this.breed = breed;
+  }
+}
