ea2cad590c34144068e53a8ffc495421b3f79fc2
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-14 14:08:47 +0200

    Write a Basic Fetch Request

--- 11th_challenge/js/app.js
+++ 11th_challenge/js/app.js
@@ -1,11 +1,13 @@
 const select = document.getElementById('breeds');
-const card = document.querySelector('.card'); 
+const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(res => res.json())
+  .then(data => console.log(data.message));
 
 
 // ------------------------------------------
