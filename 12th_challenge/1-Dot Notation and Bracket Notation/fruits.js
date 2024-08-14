2d229f4e80a99da51f756b733ed84ea92905d381
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-14 18:14:02 +0200

    basic OOJS

--- 12th_challenge/1-Dot Notation and Bracket Notation/fruits.js
+++ 12th_challenge/1-Dot Notation and Bracket Notation/fruits.js
@@ -1,1 +1,3 @@
 const fruits = ['apple', 'pear', 'strawberry'];
+fruitsLength = fruits.length; // długość
+fruits[0]; // pierwszy element
--- 12th_challenge/1-Dot Notation and Bracket Notation/pets.js
+++ 12th_challenge/1-Dot Notation and Bracket Notation/pets.js
@@ -2,7 +2,23 @@ const ernie = {
     animal: 'dog',
     age: '1',
     breed: 'pug',
-    bark: function(){
+    bark: function () {
         console.log('Woof!');
+    },
+    fullDescription: function () {
+        console.log(this.animal + ' ' + this.breed)
     }
 }
+
+console.log(ernie.age);
+console.log(ernie['age']);
+console.log(ernie.breed);
+console.log(ernie['breed']);
+
+ernie.bark();
+ernie['bark']();
+
+ernie.fullDescription();
+
+const prop = 'breed';
+ernie[prop];
