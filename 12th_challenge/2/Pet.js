7a6144d2a8341acb3e23e2376bb3ec3e3c3f3b70
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-14 19:02:37 +0200

    adding method to class

--- 12th_challenge/2/Pet.js
+++ 12th_challenge/2/Pet.js
@@ -1,13 +1,18 @@
 class Pet {
-  constructor(name, animal, age, breed) { //constructor method - special method for creating and initializing an object created with a class.
+  constructor(name, animal, age, breed, sound) { //constructor method - special method for creating and initializing an object created with a class.
     this.name = name;
     this.animal = animal;
     this.age = age;
     this.breed = breed;
+    this.sound = sound;
+  }
+
+  speak() {
+    console.log(this.sound);
   }
 }
 
-const ernie = new Pet('Ernie', 'dog', 1, 'pug');
-const valentina = new Pet('Valentina', 'dog', 8, 'akita');
+const ernie = new Pet('Ernie', 'dog', 1, 'pug', 'Woof!');
+const valentina = new Pet('Valentina', 'cat', 8, 'akita', 'Meow!');
 
 console.log(ernie, valentina);
