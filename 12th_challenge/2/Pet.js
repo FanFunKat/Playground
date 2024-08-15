6974c0b156a875955749e3ced183ae6ad6c92a4c
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-15 13:31:25 +0200

    adding simple setter method

--- 12th_challenge/2/Pet.js
+++ 12th_challenge/2/Pet.js
@@ -24,6 +24,15 @@ class Pet {
     }
   }
 
+  get owner() { //getters always return a value
+    return this._owner;
+  };
+
+  set owner(owner) { //setters always receives only 1 parameter and they don't return values. Thats why we need getter above^
+    this._owner = owner; // the name of property can never be the same as the name of a getter or setter method name - backing property, convention: _name (underscore + name of property(=to name method))
+    console.log(`setter called: ${owner}`);
+  }
+
   speak() {
     console.log(this.sound);
   }
@@ -39,3 +48,6 @@ const valentina = new Pet('Valentina', 'cat', 8, 'akita', 'Meow!');
 
 console.log(ernie.activity); // Accessing the activity property, which is a getter method, will not show up as an actual property attached to the object. Therefore, you won't see it when directly inspecting the object.
 console.log(ernie);
+
+ernie.owner = 'John';
+console.log(ernie.owner);
