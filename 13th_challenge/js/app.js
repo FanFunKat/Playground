44c7569dddd5759d18a1834a98778fd4b695ef30
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-16 16:45:54 +0200

    added startGameBtn

---
+++ 13th_challenge/js/app.js
@@ -0,0 +1,14 @@
+const game = new Game();
+
+/**
+ * Listens for click on `#begin-game` and calls startGame() on game object
+ */
+
+const startGameBtn = document.getElementById('begin-game');
+
+startGameBtn.addEventListener('click', function(){
+  game.startGame();
+
+  this.style.display = 'none';
+  document.getElementById('play-area').style.opacity = '1';
+});
