97eb95f2bb76ef7e803e93cddf5e69e6d92e3a4c
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-16 13:40:13 +0200

    Added parameters and creating token objects for player

--- 13th_challenge/js/Player.js
+++ 13th_challenge/js/Player.js
@@ -1,5 +1,26 @@
 class Player {
-    constructor() {
+    constructor(name, id, color, active = false) {
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.active = active;
+        this.token = this.createTokens(21); // 'this' - indicating that the method is a member of the Player class.
+    }
 
+    /**
+     * Creating token objects for player
+     * @param {*integer} num - number of tokens objects to be created
+     * @returns - array of token objects
+     */
+
+    createTokens(num){
+        const newTokens = []
+
+        for (let i = 0; i < num; i++) {
+            const token = new Token(i, this)
+            newTokens.push(token)
+        }
+
+        return newTokens
     }
 }
