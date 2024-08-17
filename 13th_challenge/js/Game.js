e027a9811e8e2a00092cf49ae4075bf231d13fad
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-17 13:51:33 +0200

    changed of order in 'if else if ' and added argument to moveRight

--- 13th_challenge/js/Game.js
+++ 13th_challenge/js/Game.js
@@ -40,17 +40,17 @@ class Game {
 
   /**
  * Branches code, depending on what key player presses
- * @param   {Object}    e - Keydown event object
+ * @param {Object} e - Keydown event object
  */
 
   handleKeydown(e){
     if (this.ready) {
-      if (e.key === 'ArrowDown') {
-        this.activePlayer.activeToken.moveDown();
-      } else if (e.key === 'ArrowLeft') {
+      if (e.key === 'ArrowLeft') {
         this.activePlayer.activeToken.moveLeft();
       } else if (e.key === 'ArrowRight') {
-        this.activePlayer.activeToken.moveRight();
+        this.activePlayer.activeToken.moveRight(this.board.columns);
+      }else if (e.key === 'ArrowDown') {
+        this.activePlayer.activeToken.moveDown();
       }
     }
   }
