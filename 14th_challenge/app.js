b77ab97e9b7420aaf12c182b17b1e2db7c38f6b4
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-19 21:06:12 +0200

    Problem, solution, planing

---
+++ 14th_challenge/app.js
@@ -0,0 +1,9 @@
+// Problem:
+// We need a simple way to look at a user's badge count and JavaScript point from a web browser
+// Solution:
+// Use Node.js to perform the profile look ups and server our template via HTTP
+
+// 1. Create a web server
+// 2. Handle HTTP route GET / and POST /i. e. Home
+// 3. Handle HTTP route GET /:username i.e. ,[user name]
+// 4. Function that handle the reading of file and marge in value
