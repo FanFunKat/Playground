badf410477829eb71efd3740319c7891af3ba19b
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-19 21:15:02 +0200

    sudocode

--- 14th_challenge/app.js
+++ 14th_challenge/app.js
@@ -4,6 +4,21 @@
 // Use Node.js to perform the profile look ups and server our template via HTTP
 
 // 1. Create a web server
+
 // 2. Handle HTTP route GET / and POST /i. e. Home
-// 3. Handle HTTP route GET /:username i.e. ,[user name]
+  // if url == '/' && GET
+    // show a search
+  // if url == '/' && POST
+    // redirect to /:username
+
+// 3. Handle HTTP route GET /:username i.e. .[user name]
+  // if url == '/........'
+    // get json from Treehouse
+      // on 'end'
+        // show profile
+      // on 'error'
+        // show error
+
 // 4. Function that handle the reading of file and marge in value
+  // read from file and get a string
+    // merge value in to string
