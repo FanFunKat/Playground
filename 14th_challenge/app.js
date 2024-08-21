424c37724ce3ed2e4d998c4f88e3609c4afb9b15
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-21 17:46:43 +0200

    Home Route

--- 14th_challenge/app.js
+++ 14th_challenge/app.js
@@ -7,20 +7,29 @@
 
 var http = require('http');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  setInterval(function(){
-    response.write(new Date() + "\n");
-  }, 1000);
+  homeRoute(request, response); //handler
+  // response.writeHead(200, {'Content-Type': 'text/plain'});
+  // setInterval(function(){
+  //   response.write(new Date() + "\n");
+  // }, 1000);
   //response.end('Hello World\n');
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
 
 
 // 2. Handle HTTP route GET / and POST /i. e. Home
+function homeRoute(request, response) {
   // if url == '/' && GET
+  if(request.url === '/'){
     // show a search
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Header\n');
+    response.write('Search\n');
+    response.end('Footer\n');
+  }
   // if url == '/' && POST
     // redirect to /:username
+}
 
 // 3. Handle HTTP route GET /:username i.e. .[user name]
   // if url == '/........'
