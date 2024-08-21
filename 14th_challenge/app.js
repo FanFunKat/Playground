c5233d9101b49df97cc5880b7edf3cf050295ff6
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-21 18:01:42 +0200

    split file

--- 14th_challenge/app.js
+++ 14th_challenge/app.js
@@ -1,53 +1,17 @@
-// Problem:
-// We need a simple way to look at a user's badge count and JavaScript point from a web browser
-// Solution:
-// Use Node.js to perform the profile look ups and server our template via HTTP
+let router = require('router.js');
 
-// 1. Create a web server
+// Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
+// Solution: Use Node.js to perform the profile look ups and server our template via HTTP
+
+// Create a web server
 
 let http = require('http');
 http.createServer(function (request, response) {
-  homeRoute(request, response); //handler
-  // response.writeHead(200, {'Content-Type': 'text/plain'});
-  // setInterval(function(){
-  //   response.write(new Date() + "\n");
-  // }, 1000);
-  //response.end('Hello World\n');
+  router.home(request, response); //handler
+  router.user(request, response); //handler
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
 
-
-// 2. Handle HTTP route GET / and POST /i. e. Home
-function homeRoute(request, response) {
-  // if url == '/' && GET
-  if(request.url === '/'){
-    // show a search
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Header\n');
-    response.write('Search\n');
-    response.end('Footer\n');
-  }
-  // if url == '/' && POST
-    // redirect to /:username
-}
-
-// 3. Handle HTTP route GET /:username i.e. .[user name]
-function userRoute(request, response) {
-  // if url == '/........'
-  let username = request.url.replace('/',''); // remove '/'
-  if (username.length > 0) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Header\n');
-    response.write(username + '\n');
-    response.end('Footer\n');
-    // get json from Treehouse
-      // on 'end'
-        // show profile
-      // on 'error'
-        // show error
-  }
-}
-
-// 4. Function that handle the reading of file and marge in value
+// Function that handle the reading of file and marge in value
   // read from file and get a string
     // merge value in to string
