4bba6fbb90edad013aac7584a50e727b2a07d5dd
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-25 21:39:21 +0200

    Adding Multiple Routes to the App

--- 15th_challenge/app.js
+++ 15th_challenge/app.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>This is my first Express app :)</h1>');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Example app is runing on localhost:3000!');
+});
 
