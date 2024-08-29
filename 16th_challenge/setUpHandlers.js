648219ed8f0c2e1a4c2e53de182149f12bfbf793
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-29 18:06:23 +0200

    using let & const is a solution :D

--- 16th_challenge/setUpHandlers.js
+++ 16th_challenge/setUpHandlers.js
@@ -1,9 +1,10 @@
-var buttons = document.getElementsByTagName('button');
+const buttons = document.getElementsByTagName('button');
 
-for(var i = 0; i < buttons.length; i += 1) {
-	var button = buttons[i];
-	var buttonName = button.innerHTML;
-	button.addEventListener('click', function() {
+
+for (let i = 0; i < buttons.length; i += 1) {
+	let button = buttons[i];
+	let buttonName = button.innerHTML;
+	button.addEventListener('click', function () {
 		console.log(buttonName);
 	});
 }
