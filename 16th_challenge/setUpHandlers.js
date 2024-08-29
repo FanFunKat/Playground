ed9fb268cfee7b6e8be9e84194fa29bb539c845f
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-29 18:13:46 +0200

    solution with var

--- 16th_challenge/setUpHandlers.js
+++ 16th_challenge/setUpHandlers.js
@@ -8,3 +8,17 @@ for (let i = 0; i < buttons.length; i += 1) {
 		console.log(buttonName);
 	});
 }
+
+// but if you use var (I don't understand why you use it, but...)
+
+function createHandler(name) {
+	return function () {
+		console.log(name);
+	}
+}
+
+for (let i = 0; i < buttons.length; i++) {
+	var button = buttons[i];
+	var buttonName = button.innerHTML;
+	button.addEventListener('click', createHandler(buttonName));
+}
