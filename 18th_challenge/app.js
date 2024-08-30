513d6615c66395de3042e149bc1181710fc2a3c0
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-30 13:35:32 +0200

    Refactor Using Promises

--- 18th_challenge/app.js
+++ 18th_challenge/app.js
@@ -9,22 +9,77 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 //CALL BACKS
-function getUsers(cb) {
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) return cb(err);
-    const users = JSON.parse(data);
-    return cb(null, users);
-  });
+// function getUsers(cb) {
+//   fs.readFile('data.json', 'utf8', (err, data) => {
+//     if (err) return cb(err);
+//     const users = JSON.parse(data);
+//     return cb(null, users);
+//   });
+// }
+
+// app.get('/', (req, res) => {
+//   getUsers((err, users) => {
+//     if (err) {
+//       res.render('error', { error: err });
+//     } else {
+//       res.render('index', { title: 'Users', users: users.users })
+//     }
+//   });
+// });
+
+// app.get('/:id', (req, res) => {
+//   getUser(req.params.id, (err, user) => {
+//     if (err) {
+//       res.render('error', { error: err });
+//     } else {
+//       getFollowers(user, (err, followers) => {
+//         if (err) {
+//           res.render('error', { error: err });
+//         } else {
+//           res.render('profile', { title: "Profile Page", user: user, followers: followers });
+//         }
+//       });
+//     }
+//   });
+// });
+
+
+// PROMISES
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      } else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    });
+  })
 }
 
 app.get('/', (req, res) => {
-  getUsers((err, users) => {
-    if (err) {
+  getUsers()
+    .then((users) => {
+      // throw new Error('This is a test error');
+      res.render('index', { title: 'Users', users: users.users });
+    })
+    .catch((err) => {
+      res.render('error', { error: err })
+    });
+});
+
+app.get('/:id', (req, res) => {
+  getUser(req.params.id)
+    .then((user) => {
+      return getFollowers(user);
+    })
+    .then((user, followers) => {
+      res.render('profile', { title: "Profile Page", users: user, followers: followers });
+    })
+    .catch((err) => {
       res.render('error', { error: err });
-    } else {
-      res.render('index', { title: 'Users', users: users.users })
-    }
-  });
+    });
 });
 
 
