cef078c8473a76c085518749731ff5c98f99ed2f
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-08-30 14:41:32 +0200

    Planning out a REST API

--- 19th_challenge/app.js
+++ 19th_challenge/app.js
@@ -5,4 +5,11 @@ app.get('/greetings', (req, res) => {
   res.json({ greeting: 'Hello world!' });
 });
 
+// Send a GET request to /quotes to READ(view) a list of quotes.
+// Send a GET request to /quotes/:id to READ(view) a quote.
+// Send a POST request to /quotes to CREATE(add) a new quote.
+// Send a PUT request to /quotes/:id to UPDATE(edit) a quote.
+// Send a DELETE request to /quotes/:id to to DELETE(remove) a quote.
+// Send a GET request to /quotes/quote/random to READ(view) a random quote.
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
