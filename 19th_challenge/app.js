1edc9e8b99fa2620d51b4d572c32f83585511508
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-01 21:14:58 +0200

    Create a New Quote

--- 19th_challenge/app.js
+++ 19th_challenge/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const records = require('./records');
 
+app.use(express.json());
+
 // Send a GET request to /quotes to READ(view) a list of quotes.
 app.get('/quotes', async (req, res) => {
   const quotes = await records.getQuotes();
@@ -14,6 +16,13 @@ app.get('/quotes/:id', async (req, res) => {
   res.json(quote);
 });
 // Send a POST request to /quotes to CREATE(add) a new quote.
+app.post('/quotes', async (req, res) => {
+  const newQuote = await records.createQuote({
+    quote: req.body.quote,
+    author: req.body.author
+  });
+  res.json(newQuote);
+});
 // Send a PUT request to /quotes/:id to UPDATE(edit) a quote.
 // Send a DELETE request to /quotes/:id to to DELETE(remove) a quote.
 // Send a GET request to /quotes/quote/random to READ(view) a random quote.
