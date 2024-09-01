5c0bf43b7b0c00955ff4401655e2a6ddbfb14609
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-01 22:00:23 +0200

    Edit A Quote

--- 19th_challenge/app.js
+++ 19th_challenge/app.js
@@ -48,6 +48,23 @@ app.post('/quotes', async (req, res) => {
   }
 });
 // Send a PUT request to /quotes/:id to UPDATE(edit) a quote.
+app.put('/quotes/:id', async (req, res) => {
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      quote.quote = req.body.quote;
+      quote.author = req.body.author;
+
+      await records.updateQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: `Quote ID ${req.params.id} not found` });
+    }
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 // Send a DELETE request to /quotes/:id to to DELETE(remove) a quote.
 // Send a GET request to /quotes/quote/random to READ(view) a random quote.
 
