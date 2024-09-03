d8458afede5c4f6fa0d4e41317ce71ac7afae1cf
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-03 23:08:33 +0200

    added new entry

--- 20th_challenge/app.js
+++ 20th_challenge/app.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'movies.db'
+  storage: 'movies.db',
+  //logging: false // disable logging
 });
 
 // Movie model
@@ -19,13 +20,21 @@ Movie.init({
   await sequelize.sync({ forse: true });
   try {
     // Instance of the Movie class represents a database row
-    const movie = await Movie.create({
+    // const movie =
+    await Movie.create({
       title: 'The Godfather',
       year: 1972,
       director: 'Francis Ford Coppola',
       rating: 9.2,
     });
-    console.log(movie.toJSON());
+    // console.log(movie.toJSON());
+
+    await Movie.create({
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      rating: 9.3,
+    });
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
