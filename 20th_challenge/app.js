30d0c6235d9b406ced7585c934e9e934209e3f65
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-03 19:58:49 +0200

    Synchronize Models with the Database

--- 20th_challenge/app.js
+++ 20th_challenge/app.js
@@ -4,11 +4,22 @@ const sequelize = new Sequelize({
   storage: 'movies.db'
 });
 
+// Movie model
+class Movie extends Sequelize.Model { }
+Movie.init({
+  title: Sequelize.STRING,
+  year: Sequelize.INTEGER,
+  director: Sequelize.STRING,
+  rating: Sequelize.INTEGER,
+}, { sequelize }
+);
+
 // async IIFE
 (async () => {
+  await sequelize.sync();
   try {
-    await sequelize.authenticate();
-    console.log('Connection to the database successful!');
+    await sequelize.authenticate({ forse: true });
+    // console.log('Connection to the database successful!');
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
