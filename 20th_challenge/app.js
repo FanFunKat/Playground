06f5a9ceb6df7401e42de4d070fa9b51ffb87af9
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-03 23:15:59 +0200

    Promise.all()

--- 20th_challenge/app.js
+++ 20th_challenge/app.js
@@ -19,22 +19,38 @@ Movie.init({
 (async () => {
   await sequelize.sync({ forse: true });
   try {
-    // Instance of the Movie class represents a database row
-    // const movie =
-    await Movie.create({
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      rating: 9.2,
-    });
-    // console.log(movie.toJSON());
-
-    await Movie.create({
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      rating: 9.3,
-    });
+    const movieInstance = await Promise.all([
+      Movie.create({
+        title: 'The Shawshank Redemption',
+        year: 1994,
+        director: 'Frank Darabont',
+        rating: 9.3,
+      }),
+      Movie.create({
+        title: 'The Godfather',
+        year: 1972,
+        director: 'Francis Ford Coppola',
+        rating: 9.2,
+      }),
+      Movie.create({
+        title: 'The Dark Knight',
+        year: 2008,
+        director: 'Christopher Nolan',
+        rating: 9.0,
+      }),
+      Movie.create({
+        title: 'The Godfather: Part II',
+        year: 1974,
+        director: 'Francis Ford Coppola',
+        rating: 9.0,
+      }),
+      Movie.create({
+        title: '12 Angry Men',
+        year: 1957,
+        director: 'Sidney Lumet',
+        rating: 8.9,
+      }),
+    ]);
   } catch (error) {
     console.error('Error connecting to the database: ', error);
   }
