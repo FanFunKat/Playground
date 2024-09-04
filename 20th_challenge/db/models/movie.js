3229f729c40083c698e106b1b062af89f6a63b43
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-04 16:02:13 +0200

    defined data types

--- 20th_challenge/db/models/movie.js
+++ 20th_challenge/db/models/movie.js
@@ -4,10 +4,13 @@ module.exports = (sequelize) => {
   // Movie model
   class Movie extends Sequelize.Model { }
   Movie.init({
-    title: Sequelize.STRING,
-    year: Sequelize.INTEGER,
-    director: Sequelize.STRING,
-    rating: Sequelize.INTEGER,
+    title: { type: Sequelize.STRING },
+    year: { type: Sequelize.DATE },
+    director: { type: Sequelize.STRING },
+    rating: { type: Sequelize.FLOAT },
+    runtime: { type: Sequelize.INTEGER },
+    releaseDate: { type: Sequelize.DATEONLY },
+    isAvailableOnVHS: { type: Sequelize.BOOLEAN }
   }, { sequelize });
 
   return Movie;
