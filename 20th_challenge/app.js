cfac1442d8d98889043bdf4138577fa1440538e9
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-04 18:17:46 +0200

    version with 'Promise.all()'

--- 20th_challenge/app.js
+++ 20th_challenge/app.js
@@ -8,41 +8,79 @@ const { Movie } = db.models;
   try {
     await db.sequelize.authenticate();
     console.log('Connection to the database successful!');
-    // const movieInstances = await Promise.all([})
-    // const moviesJSON = movieInstances.map(movie => movie.toJSON());
-    // console.log(moviesJSON);
-    const movie = await Movie.create({
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      rating: 9.3,
-      runtime: 142,
-      releaseDate: '1994-10-14',
-      isAvailableOnVHS: true,
-    });
-    console.log(movie.toJSON());
+    const movieInstances = await Promise.all([
+      Movie.create({
+        title: 'The Shawshank Redemption',
+        year: 1994,
+        director: 'Frank Darabont',
+        rating: 9.3,
+        runtime: 142,
+        releaseDate: '1994-10-14',
+        isAvailableOnVHS: true,
+      }),
+      Movie.create({
+        title: 'The Godfather',
+        year: 1972,
+        director: 'Francis Ford Coppola',
+        rating: 9.2,
+        runtime: 175,
+        releaseDate: '1972-03-24',
+        isAvailableOnVHS: false,
+      }),
+      Movie.create({
+        title: 'The Dark Knight',
+        year: 2008,
+        director: 'Christopher Nolan',
+        rating: 9.0,
+        runtime: 152,
+        releaseDate: '2008-07-18',
+        isAvailableOnVHS: true,
+      }),
+      Movie.create({
+        title: 'The Godfather: Part II',
+        year: 1974,
+        director: 'Francis Ford Coppola',
+        rating: 9.0,
+        runtime: 202,
+        releaseDate: '1974-12-20',
+        isAvailableOnVHS: true,
+      })
+    ])
+    const moviesJSON = movieInstances.map(movie => movie.toJSON());
+    console.log(moviesJSON);
 
-    const movie2 = await Movie.create({
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      rating: 9.2,
-      runtime: 175,
-      releaseDate: '1972-03-24',
-      isAvailableOnVHS: false,
-    });
-    console.log(movie2.toJSON());
+    // const movie = await Movie.create({
+    //   title: 'The Shawshank Redemption',
+    //   year: 1994,
+    //   director: 'Frank Darabont',
+    //   rating: 9.3,
+    //   runtime: 142,
+    //   releaseDate: '1994-10-14',
+    //   isAvailableOnVHS: true,
+    // });
+    // console.log(movie.toJSON());
 
-    const movie3 = await Movie.create({
-      title: 'The Dark Knight',
-      year: 2008,
-      director: 'Christopher Nolan',
-      rating: 9.0,
-      runtime: 152,
-      releaseDate: '2008-07-18',
-      isAvailableOnVHS: true,
-    })
-    console.log(movie3.toJSON());
+    // const movie2 = await Movie.create({
+    //   title: 'The Godfather',
+    //   year: 1972,
+    //   director: 'Francis Ford Coppola',
+    //   rating: 9.2,
+    //   runtime: 175,
+    //   releaseDate: '1972-03-24',
+    //   isAvailableOnVHS: false,
+    // });
+    // console.log(movie2.toJSON());
+
+    // const movie3 = await Movie.create({
+    //   title: 'The Dark Knight',
+    //   year: 2008,
+    //   director: 'Christopher Nolan',
+    //   rating: 9.0,
+    //   runtime: 152,
+    //   releaseDate: '2008-07-18',
+    //   isAvailableOnVHS: true,
+    // })
+    // console.log(movie3.toJSON());
 
   } catch (error) {
     console.error('Error connecting to the database: ', error);
