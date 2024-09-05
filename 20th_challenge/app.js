2734b8282a0aee87a69da49017ccdf7f084f9dc0
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-05 15:06:42 +0200

    new record with build() method

--- 20th_challenge/app.js
+++ 20th_challenge/app.js
@@ -62,6 +62,19 @@ const { Movie, Person } = db.models;
     const peopleJSON = personInstances.map(person => person.toJSON());
     console.log(peopleJSON);
 
+    // New instance
+    //build() is useful when you need to manipulate instances in any way before storing them. build() would also be useful when you need a model instance to bind to a template (for example, a "New Record" form or page). You can create and then save a database object from the data bound to the template.
+    const movie5 = await Movie.build({ //stores data in memory only
+      title: 'Toy Story 3',
+      director: 'Lee Unkrich',
+      rating: 8.3,
+      runtime: 103,
+      releaseDate: '2010-06-18',
+      isAvailableOnVHS: false,
+    });
+    await movie5.save(); // save the record
+    console.log(movie5.toJSON());
+
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
