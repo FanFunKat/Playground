f2074ec23e69b148be29f0f72e1f239b78ef13a6
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-06 14:56:19 +0200

    Added instance method (with moment) publishedAt() & shortDescription()

--- 21st_challenge/models/article.js
+++ 21st_challenge/models/article.js
@@ -1,8 +1,18 @@
 'use strict';
 const { DataTypes, Model } = require('sequelize');
+const moment = require('moment');
 
 module.exports = (sequelize) => {
-  class Article extends Model { }
+  class Article extends Model {
+    publishedAt() {
+      const date = moment(this.createdAt).format('MMMM D, YYYY, h:mma');
+      return date;
+    }
+    shortDescription() {
+      const shortDesc = this.body.length > 200 ? this.body.substring(0, 200) + '...' : this.body;
+      return shortDesc;
+    }
+  }
   Article.init({
     title: DataTypes.STRING,
     author: DataTypes.STRING,
