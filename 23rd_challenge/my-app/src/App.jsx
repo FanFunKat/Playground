ba5f0fd6318d5c4646424c0fefe1248fc7d18321
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-08 17:57:27 +0200

    solved chalenged 23

--- 23rd_challenge/my-app/src/App.jsx
+++ 23rd_challenge/my-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Planet } from "./components/planet.jsx";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('/planets.json')
+      .then(response => response.json())
+      .then(data => setPlanets(data))
+      .catch(error => console.error('Error fetching planets data:', error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      {planets.map(planet => (
+        <Planet
+          key={planet.name}
+          name={planet.name}
+          url={planet.url}
+          desc={planet.desc}
+          diameter={planet.diameter}
+          moons={planet.moons}
+        />
+      ))}
+    </div>
+  );
 }
-
-export default App
---
+++ 23rd_challenge/my-app/src/components/planet.jsx
@@ -0,0 +1,16 @@
+export function Planet({ name, diameter, moons, desc, url }) {
+  return (
+    <div className="card">
+      <div>
+        <img src={url} alt={name} />
+      </div>
+      <h2>{name}</h2>
+      <p>{desc}</p>
+      <h3>Planet Profile</h3>
+      <ul>
+        <li><strong>Diameter:</strong> {diameter}</li>
+        <li><strong>Moons:</strong> {moons}</li>
+      </ul>
+    </div>
+  )
+}
