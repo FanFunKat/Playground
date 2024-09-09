d52c188a59e261e377079b1da076d5f2ae3261c8
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-09 16:15:20 +0200

    Controlled Components

--- 24th_challenge/my-app/src/components/AddPlayerForm.jsx
+++ 24th_challenge/my-app/src/components/AddPlayerForm.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function AddPlayerForm() {
+  const [value, setValue] = useState("");
+
   return (
     <form>
       <input
         type="text"
+        value={value}
         placeholder="Enter a player's name"
+        onChange={event => setValue(event.target.value)}
       />
       <input
         type="submit"
