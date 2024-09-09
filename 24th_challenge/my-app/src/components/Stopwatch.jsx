7c486eb44e763e017003f4f1f949ff433e46101f
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-09 17:13:05 +0200

    Updating the Stopwatch State Using the Effect Hook

--- 24th_challenge/my-app/src/components/Stopwatch.jsx
+++ 24th_challenge/my-app/src/components/Stopwatch.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function Stopwatch() {
   const [isRuning, setIsRuning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let id;
+    if (isRuning) {
+      id = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 1);
+      }, 1000);
+      return () => clearInterval(id);
+    }
+  }, [isRuning])
 
   return (
     <div className="stopwatch">
       <h2>Stopwatch</h2>
-      <span className="stopwatch-time">0</span>
+      <span className="stopwatch-time">{elapsedTime}</span>
       <button onClick={() => setIsRuning((prevValue) => !prevValue)}>
         {isRuning ? "Stop" : "Start"}
       </button>
-      <button>Reset</button>
+      <button onClick={() => setElapsedTime(0)}>Reset</button>
     </div>
   );
 }
