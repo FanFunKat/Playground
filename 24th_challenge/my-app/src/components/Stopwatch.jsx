f97c1d8e33b973b95e4412c19bc763472b0c0cbb
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-09 16:59:49 +0200

    Stopwatch State

--- 24th_challenge/my-app/src/components/Stopwatch.jsx
+++ 24th_challenge/my-app/src/components/Stopwatch.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 export function Stopwatch() {
+  const [isRuning, setIsRuning] = useState(false);
 
   return (
     <div className="stopwatch">
       <h2>Stopwatch</h2>
       <span className="stopwatch-time">0</span>
-      <button>Start</button>
+      <button onClick={() => setIsRuning((prevValue) => !prevValue)}>
+        {isRuning ? "Stop" : "Start"}
+      </button>
       <button>Reset</button>
     </div>
   );
