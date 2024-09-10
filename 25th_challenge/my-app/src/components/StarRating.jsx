677a626968934d5eaccc1deb8e61b23118a888ad
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-10 21:30:39 +0200

    my solution

--- 25th_challenge/my-app/src/components/StarRating.jsx
+++ 25th_challenge/my-app/src/components/StarRating.jsx
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
+import Star from './Star';
 
 const StarRating = () => {
 	// Initialize a 'courseRating' state
+	const [courseRating, setCourseRating] = useState(0);
 
 	// Write a function that returns 5 Star components
+	const renderStars = () => {
+		const starsArray = [];
+		for (let i = 0; i < 5; i++) {
+			starsArray.push(<Star
+				key={i}
+				isSelected={i <= courseRating}
+				handleClick={() => updateRating(i)}
+			/>);
+		}
+		return starsArray;
+	};
 
-
-	// Write an event handler that updates the courseRating state.
+	// Write an event handler that updates the courseRating state based on the star a user clicks.That function will be passed down to each Star component via props and called when a user clicks a star.
 	// Pass the function to a Star component via props
 
+	const updateRating = (rating) => {
+		setCourseRating(rating);
+	};
 
 	return (
 		<ul className='course--stars'>
 			{/* Render the Star components */}
-
+			{renderStars()}
 		</ul>
 	);
 }
