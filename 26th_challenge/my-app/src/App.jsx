9d56e6c16dac3d89de4899df9514450e21724fdf
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-11 16:10:39 +0200

    fetching data

--- 26th_challenge/my-app/src/App.jsx
+++ 26th_challenge/my-app/src/App.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchForm from './Components/SearchForm';
 import GifList from "./Components/GifList";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+  useEffect(() => {
+    fetch("https://api.giphy.com/v1/gifs/trending?api_key=r3EcdDb2hI0q2TRbgnvBje8G8h7FT5J2&limit=24&rating=g")
+      .then(response => response.json())
+      .then(responseData => { setGifs(responseData.data) })
+      .catch(error => {
+        console.error("Error fetching and parsing data:", error)
+      });
+  }, []);
+
   return (
     <div>
       <div className="main-header">
