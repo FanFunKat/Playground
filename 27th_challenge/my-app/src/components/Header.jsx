02fdc949daf8b5a5d5e197d2971eaf63c9cd2551
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-13 22:06:14 +0200

    Descendant routes

--- 27th_challenge/my-app/src/components/Header.jsx
+++ 27th_challenge/my-app/src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink to="/" end style={({ isActive }) => isActive ? { background: 'tomato' } : undefined}>Home</NavLink></li>
-      <li><NavLink to="about" className={(isActive) => isActive ? "custom-class-name" : undefined}>About</NavLink></li>
+      <li><NavLink to="/" end >Home</NavLink></li>
+      <li><NavLink to="about" >About</NavLink></li>
       <li><NavLink to="teachers">Teachers</NavLink></li>
       <li><NavLink to="courses">Courses</NavLink></li>
     </ul>
