1a2cab918f03e077a18907e60f9e4bf989b7a490
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-14 20:52:03 +0200

    UserContext

--- 28th_challenge/my-app/src/App.jsx
+++ 28th_challenge/my-app/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react"
+import { Route, Routes } from "react-router-dom"
+import { UserContext } from "./context/UserContrxt"
 
 // App Components
-import Header from "./components/Header";
-import Home from "./components/Home";
-import UserSignIn from "./components/UserSignIn";
-import UserSignOut from "./components/UserSignOut";
-import Settings from "./components/Settings";
-import NotFound from "./components/NotFound";
+import Header from "./components/Header"
+import Home from "./components/Home"
+import UserSignIn from "./components/UserSignIn"
+import UserSignOut from "./components/UserSignOut"
+import Settings from "./components/Settings"
+import NotFound from "./components/NotFound"
 
 function App() {
   const [user, setUser] = useState(null);
@@ -42,31 +43,33 @@ function App() {
   }
 
   return (
-    <div>
-      <Header
-        user={user}
-        accentColor={accentColor} />
-      <Routes>
-        <Route path="/" element={<Home user={user} />} />
-        <Route path="signin" element={
-          <UserSignIn
-            signIn={signInUser}
-            accentColor={accentColor} />
-        } />
-        <Route path="signout" element={<UserSignOut signOut={signOutUser} />} />
-        <Route path="settings" element={
-          <Settings
-            user={user}
-            isDarkMode={isDarkMode}
-            toggleDarkMode={toggleDarkMode}
-            accentColor={accentColor}
-            updateAccentColor={setAccentColor}
-            fontPercentage={fontPercentage}
-            updateFontPercentage={setFontPercentage} />
-        } />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <UserContext.Provider>
+      <div>
+        <Header
+          user={user}
+          accentColor={accentColor} />
+        <Routes>
+          <Route path="/" element={<Home user={user} />} />
+          <Route path="signin" element={
+            <UserSignIn
+              signIn={signInUser}
+              accentColor={accentColor} />
+          } />
+          <Route path="signout" element={<UserSignOut signOut={signOutUser} />} />
+          <Route path="settings" element={
+            <Settings
+              user={user}
+              isDarkMode={isDarkMode}
+              toggleDarkMode={toggleDarkMode}
+              accentColor={accentColor}
+              updateAccentColor={setAccentColor}
+              fontPercentage={fontPercentage}
+              updateFontPercentage={setFontPercentage} />
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
