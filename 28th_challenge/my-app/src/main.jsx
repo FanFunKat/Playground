f02957f332af728a327aabf4a587433e5bbd959a
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-09-15 09:35:56 +0200

    UserProvider

--- 28th_challenge/my-app/src/main.jsx
+++ 28th_challenge/my-app/src/main.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { UserProvider } from './context/UserContrxt';
 
 import './styles/global.css';
 import './styles/new.css';
@@ -9,7 +10,9 @@ import './styles/new.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 )
