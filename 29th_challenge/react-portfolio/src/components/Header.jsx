4901f83bb8da1f0457f731c09a3ff775492577b8
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-07 00:25:09 +0200

    Header component

---
+++ 29th_challenge/react-portfolio/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import menu from '../assets/open_menu.svg'
+
+const Header = () => {
+  return (
+    <header>
+      <span>Katarzyna</span>
+      <button className='menu-btn'>
+        <img src={menu} alt='menu' />
+      </button>
+      <nav className='nav'>
+        <ul>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
