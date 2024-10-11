5751e855d1328396dd1d25647d7e258974036a36
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-11 14:21:15 +0200

    destructuring and annotations

--- 30th_challenge/annotations/function.ts
+++ 30th_challenge/annotations/function.ts
@@ -20,3 +20,20 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+// const logWeather = (forecast: { date: Date; weather: string }): void => {
+//   console.log(forecast.date);
+//   console.log(forecast.weather);
+// };
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
