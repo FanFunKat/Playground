70b57f63517cc3f7a33bd3f9fe44b268096b3919
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-11 16:35:27 +0200

    refactoring

--- 30th_challenge/annotations/interfaces.ts
+++ 30th_challenge/annotations/interfaces.ts
@@ -1,22 +1,31 @@
 //interface, new type whit generic name
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
 // new object
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
 };
 
 // function
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(drink);
