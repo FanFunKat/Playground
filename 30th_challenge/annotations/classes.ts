ba9b1c64cbcb58348951aab7851661d855f16d09
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-11 17:28:34 +0200

    Fields with inheritance

--- 30th_challenge/annotations/classes.ts
+++ 30th_challenge/annotations/classes.ts
@@ -1,10 +1,18 @@
 class Vehicle {
+  constructor(public color: string) { }
+
   protected honk(): void {
     console.log("beep");
   }
 }
 
+const vehicle = new Vehicle('orange');
+
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log("vroom");
   }
@@ -15,6 +23,5 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+const carNew = new Car(4, 'red');
+carNew.startDrivingProcess();
