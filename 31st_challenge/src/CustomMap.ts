13e32124f5e76554644cd915a65dcdc6bf2ec2ed
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-16 23:19:59 +0200

    Added markers

--- 31st_challenge/src/CustomMap.ts
+++ 31st_challenge/src/CustomMap.ts
@@ -1,3 +1,14 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -14,5 +25,15 @@ export class CustomMap {
     } else {
       console.error("The 'map' element was not found.");
     }
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
   }
 }
--- 31st_challenge/src/index.ts
+++ 31st_challenge/src/index.ts
@@ -1,18 +1,21 @@
 /// <reference types="@types/google.maps" />
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-// const user = new User();
-// console.log(user);
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-// const company = new Company();
-// console.log(company);
+customMap.addMarker(user)
+customMap.addMarker(company);
 
-window.onload = function () {
-  if (typeof google !== 'undefined' && google.maps) {
-    new CustomMap('map');
-  } else {
-    console.error("Google Maps API was not loaded.");
-  }
-};
+
+// window.onload = function () {
+//   if (typeof google !== 'undefined' && google.maps) {
+//     new CustomMap('map');
+//   } else {
+//     console.error("Google Maps API was not loaded.");
+//   }
+// };
+
