e8b5995f41401867601cae75b0f4a5edc0b22c64
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-16 13:34:17 +0200

    display google maps

--- 31st_challenge/src/index.ts
+++ 31st_challenge/src/index.ts
@@ -1,8 +1,29 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
+
+function initMap() {
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    new google.maps.Map(mapElement, {
+      zoom: 1,
+      center: { lat: 0, lng: 0 }
+    });
+  } else {
+    console.error("Element 'map' nie został znaleziony.");
+  }
+}
+
+window.onload = function () {
+  if (typeof google !== 'undefined' && google.maps) {
+    initMap();
+  } else {
+    console.error("Google Maps API nie zostało załadowane.");
+  }
+};
