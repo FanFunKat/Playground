03bc2bea738ecdf031ba8553f17ec92664f0b875
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-21 13:19:32 +0200

    CsvFileReader.ts

--- 33th_challenge/src/index.ts
+++ 33th_challenge/src/index.ts
@@ -1,10 +1,7 @@
-import fs from 'fs'; // npm install @types/node
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-  return row.split(',');
-});
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 // enum - enumeration - collection of clously related values
 enum MatchResult {
@@ -15,7 +12,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
