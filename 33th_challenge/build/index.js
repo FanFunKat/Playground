c448738cb19c399711e1230ed7e423700902c27a
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-21 12:17:31 +0200

    loading data

--- 33th_challenge/build/index.js
+++ 33th_challenge/build/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs")); // npm install @types/node
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+});
--- 33th_challenge/src/index.ts
+++ 33th_challenge/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import fs from 'fs'; // npm install @types/node
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+});
+
