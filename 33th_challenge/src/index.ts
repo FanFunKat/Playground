03fbda103e9e4660f23c0532e814729a59c671d1
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-22 19:19:12 +0200

    WinsAnalysis.ts

--- 33th_challenge/src/index.ts
+++ 33th_challenge/src/index.ts
@@ -1,6 +1,6 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 
 // Create an object that satisfies the 'DataReader' interface
 
@@ -13,14 +13,14 @@ matchReader.load();
 
 // matchReader.matches
 
-let manUnitedWins = 0;
+// let manUnitedWins = 0;
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
+// for (let match of matchReader.matches) {
+//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
 
-console.log(`Man United won ${manUnitedWins} games`);
+// console.log(`Man United won ${manUnitedWins} games`);
