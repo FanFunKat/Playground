871b215ca7e958d741609525e6e0b7a2194f4236
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-27 00:44:11 +0200

    User model start

--- 34th_challenge/my-app/src/main.ts
+++ 34th_challenge/my-app/src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+import { User } from './models/User';
+
+const user = new User({});
+
+user.set({ age: 9999 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
