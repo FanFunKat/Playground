a513aeeb9d4a78eb181e9f2e4e2c30de76915bfb
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-27 13:37:02 +0100

    on() method in User class

--- 34th_challenge/my-app/src/main.ts
+++ 34th_challenge/my-app/src/main.ts
@@ -2,7 +2,5 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.set({ age: 9999 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => { });
+console.log(user);
--- 34th_challenge/my-app/src/models/User.ts
+++ 34th_challenge/my-app/src/models/User.ts
@@ -3,14 +3,24 @@ interface UserProps {
   age?: number;
 }
 
-export class User {
-  constructor(private data: UserProps) { }
+type Callback = () => void;
 
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) { }
+  // K - key
   get<K extends keyof UserProps>(propName: K): UserProps[K] {
     return this.data[propName];
   }
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
