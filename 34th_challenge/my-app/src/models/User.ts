00d3e9c9e50ed5b408297d37838cae59f66f425e
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-28 13:03:05 +0100

    save method

--- 34th_challenge/my-app/src/models/User.ts
+++ 34th_challenge/my-app/src/models/User.ts
@@ -46,4 +46,15 @@ export class User {
       });
   }
 
+  save(): void {
+    const id = this.get('id');
+
+    if (this.get('id')) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      // post
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
 }
