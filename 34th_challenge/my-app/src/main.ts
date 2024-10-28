80096182a7605aa4785a730c5897a3ee2f770b76
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-10-28 12:57:44 +0100

    fetch method

--- 34th_challenge/my-app/src/main.ts
+++ 34th_challenge/my-app/src/main.ts
@@ -1,9 +1,15 @@
 import { User } from './models/User';
 
-const user = new User({});
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-  console.log('Change #1')
-});
+user.fetch();
 
-user.trigger('change');
+setTimeout(() => {
+  console.log(user);
+}, 4000);
+
+// user.on('change', () => {
+//   console.log('Change #1')
+// });
+
+// user.trigger('change');
