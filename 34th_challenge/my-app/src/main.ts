1b0d018659f661bf405da24575d20206a661b65f
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-11-01 22:15:47 +0100

    handling events in Eventing class

--- 34th_challenge/my-app/src/main.ts
+++ 34th_challenge/my-app/src/main.ts
@@ -1,15 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'NAME', age: 4 });
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 4000);
-
-// user.on('change', () => {
-//   console.log('Change #1')
-// });
-
-// user.trigger('change');
+user.save();
