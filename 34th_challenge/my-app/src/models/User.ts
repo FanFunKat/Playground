8ea21ad3da4a44820f17f2132800c94b11f21786
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-11-02 16:38:49 +0100

    Class Sync start

--- 34th_challenge/my-app/src/models/User.ts
+++ 34th_challenge/my-app/src/models/User.ts
@@ -20,22 +20,4 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (this.get('id')) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // post
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
 }
