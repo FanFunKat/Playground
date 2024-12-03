cc326d26a03a0c9c3efc03cf41cb597c4bce99c7
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-12-03 22:42:22 +0100

    Express basic routes

--- 35th_challenge/server/src/index.ts
+++ 35th_challenge/server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>'Hello World!'</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
