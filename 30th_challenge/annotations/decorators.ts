6e6595c45189b7c182403601ef99d64bb4364d72
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-12-12 22:36:45 +0100

    alternative to decorator

--- 30th_challenge/annotations/decorators.ts
+++ 30th_challenge/annotations/decorators.ts
@@ -5,7 +5,7 @@ class Boat {
     return `This boat color is ${this.color}`;
   }
 
-  @testDecorator
+  // @testDecorator //alternative to line 19
   pilot(): void {
     console.log('swish')
   };
@@ -15,3 +15,5 @@ function testDecorator(target: any, key: string): void {
   console.log('Target:', target);
   console.log('Key:', key);
 }
+
+testDecorator(Boat.prototype, 'pilot');
