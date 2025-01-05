728748ab99a2e524268f592c60b3615c35e3e873
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-05 12:26:58 +0100

    props, destructuring

--- 37th_challenge/my-app/src/components/ProfileCard.jsx
+++ 37th_challenge/my-app/src/components/ProfileCard.jsx
@@ -1,3 +1,17 @@
-export function ProfileCard() {
-  return <div>Profile Card</div>
+// export function ProfileCard(props) {
+//   return (
+//     <div>
+//       <div>Title is {props.title}</div>
+//       <div>Handle is {props.handle}</div>
+//     </div>
+//   )
+// }
+
+export function ProfileCard({ title, handle }) { //destructuring
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  )
 }
