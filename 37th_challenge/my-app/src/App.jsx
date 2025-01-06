58bb3bd2ffa648b20964ef720af98cfdc60f1b46
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-06 13:26:00 +0100

    handle images

--- 37th_challenge/my-app/src/App.jsx
+++ 37th_challenge/my-app/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import { ProfileCard } from './components/ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+
 
 function App() {
   return (
     <div>
+      <img src={AlexaImage} />
+      <img src={'/images/siri.png'} />
       <div>Personal Digital Assistants</div>
       <ProfileCard title='Alexsa' handle='@alexa99' />
       <ProfileCard title='Cortana' handle='@cortana32' />
