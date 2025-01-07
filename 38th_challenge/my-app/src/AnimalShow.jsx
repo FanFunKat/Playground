ebebb1bef8b377ab32c292af1bc21136609a7f36
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-07 21:38:53 +0100

    loading & showing svg

--- 38th_challenge/my-app/src/AnimalShow.jsx
+++ 38th_challenge/my-app/src/AnimalShow.jsx
@@ -1,5 +1,16 @@
+const svgMap = {
+  dog: 'svg/dog.svg',
+  cat: 'svg/cat.svg',
+  cow: 'svg/cow.svg',
+  bird: 'svg/bird.svg',
+  gator: 'svg/gator.svg',
+  horse: 'svg/horse.svg',
+}
+
 export function AnimalShow({ type }) {
   return (
-    <div>{type}</div>
+    <div>
+      <img src={svgMap[type]} alt="animal" />
+    </div>
   )
 }
