708ac74dacc5b29975dfe2a139e28b6ad4381431
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-12 14:06:43 +0100

    handleSubmit async:await

--- 39th_challenge/my-app/src/App.jsx
+++ 39th_challenge/my-app/src/App.jsx
@@ -3,8 +3,9 @@ import { searchImages } from "./api";
 
 function App() {
 
-  const handleSubmit = (term) => {
-    searchImages(term);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    console.log(result)
   };
 
   return (
