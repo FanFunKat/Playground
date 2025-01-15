031ee1766398d3af7b816c4cb6ad6de9a1dd602b
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-15 21:57:06 +0100

    adding book to array

--- 40th_chllenge/books/src/App.jsx
+++ 40th_chllenge/books/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
+    const updatedBooks = [
+      ...books, { id: 123, title }
+    ]
+    setBooks(updatedBooks);
   }
 
   return (
