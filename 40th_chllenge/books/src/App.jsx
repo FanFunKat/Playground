47098055aa56a9b7955f091c4282801252e6ee28
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-15 22:07:56 +0100

    id -  Math .random - not perfect

--- 40th_chllenge/books/src/App.jsx
+++ 40th_chllenge/books/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   const createBook = (title) => {
     const updatedBooks = [
-      ...books, { id: 123, title }
+      ...books, {
+        id: Math.round(Math.random() * 9999),
+        title
+      }
     ]
     setBooks(updatedBooks);
   }
