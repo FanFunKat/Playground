a522f97fb3295cd0129aa35f225fb6eb674260bf
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-16 22:57:53 +0100

    BookEdit - default form values

--- 40th_chllenge/books/src/components/BookEdit.jsx
+++ 40th_chllenge/books/src/components/BookEdit.jsx
@@ -1,5 +1,23 @@
-export function BookEdit() {
+import { useState } from "react";
+
+export function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (e) => {
+    setTitle(e.taeget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Book edit</div>
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">
+        Save
+      </button>
+    </form>
   )
 }
