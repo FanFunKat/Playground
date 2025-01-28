e2bc9b6902f83e149168e5a21d93392da12f29dd
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-28 21:38:05 +0100

    added icons to buttons

--- 41st_challenge/comps/src/App.jsx
+++ 41st_challenge/comps/src/App.jsx
@@ -1,13 +1,30 @@
 import { Button } from './components/Button'
+import { GoBell, GoAlert, GoThumbsup } from "react-icons/go";
+
 
 function App() {
   return (
     <>
-      <div><Button primary rounded>Primary</Button></div>
+      <div>
+        <Button primary rounded>
+          <GoBell />
+          Primary
+        </Button>
+      </div>
       <div><Button secondary>Secondary</Button></div>
-      <div><Button success>Success</Button></div>
+      <div>
+        <Button success>
+          <GoThumbsup />
+          Success
+        </Button>
+      </div>
       <div><Button warning>Warning</Button></div>
-      <div><Button danger outline>Danger</Button></div>
+      <div>
+        <Button danger outline>
+          <GoAlert />
+          Danger
+        </Button>
+      </div>
     </>
   );
 }
