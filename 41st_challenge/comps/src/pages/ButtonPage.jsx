8e3e6cbdd571b8b2335f2a6f88dcde5c4a5626d8
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-01-31 07:03:20 +0100

    added ButtonPage

---
+++ 41st_challenge/comps/src/pages/ButtonPage.jsx
@@ -0,0 +1,29 @@
+import { Button } from '../components/Button'
+import { GoBell, GoAlert, GoThumbsup } from "react-icons/go";
+
+export function ButtonPage() {
+  return (
+    <>
+      <div>
+        <Button primary rounded className="mb-5">
+          <GoBell />
+          Primary
+        </Button>
+      </div>
+      <div><Button secondary>Secondary</Button></div>
+      <div>
+        <Button success>
+          <GoThumbsup />
+          Success
+        </Button>
+      </div>
+      <div><Button warning>Warning</Button></div>
+      <div>
+        <Button danger outline>
+          <GoAlert />
+          Danger
+        </Button>
+      </div>
+    </>
+  );
+}
