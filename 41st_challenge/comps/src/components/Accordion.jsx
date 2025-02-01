670c40852885ebc045ff142cd1f3415a0856a10a
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-01 17:46:42 +0100

    handleClick with map

--- 41st_challenge/comps/src/components/Accordion.jsx
+++ 41st_challenge/comps/src/components/Accordion.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 
 export function Accordion({ items }) {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const onTitleClick = (index) => {
-    setActiveIndex(index);
-  };
+  const [activeIndex, setActiveIndex] = useState(null);
 
   const renderedItems = items.map((item, index) => {
     const isActive = index === activeIndex;
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => setActiveIndex(index)}>{item.label}</div>
         {isActive && <div>{item.content}</div>}
       </div>
     );
