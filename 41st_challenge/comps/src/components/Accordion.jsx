d7808602e1227fc53396f1775aab2b55b25771ce
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-01 17:27:47 +0100

    useState activeIndex

--- 41st_challenge/comps/src/components/Accordion.jsx
+++ 41st_challenge/comps/src/components/Accordion.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 export function Accordion({ items }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isActive = index === activeIndex ? "active" : "";
+
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+
+      </div>
+    );
+  });
+
   return (
     <div>
-      {items.map((item) => (
-        <div key={item.id}>
-          <div>{item.label}</div>
-          <div>{item.content}</div>
-        </div>
-      ))}
+      {renderedItems}
     </div>
   );
 }
