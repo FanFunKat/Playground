48e44692c4197a54771fffb891def3821bcbe5cc
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-01 18:02:43 +0100

    Longhand version - only with map f when handleClick is defined outside the maping f

--- 41st_challenge/comps/src/components/Accordion.jsx
+++ 41st_challenge/comps/src/components/Accordion.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 export function Accordion({ items }) {
   const [activeIndex, setActiveIndex] = useState(null);
 
+  const handleClick = (nextIndex) => {
+    setActiveIndex(nextIndex);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isActive = index === activeIndex;
 
     return (
       <div key={item.id}>
-        <div onClick={() => setActiveIndex(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
         {isActive && <div>{item.content}</div>}
       </div>
     );
