155e799b2442987b6c04eaccbaf20fa354425424
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-02 12:53:24 +0100

    Conditional icon rendering - display icons

--- 41st_challenge/comps/src/components/Accordion.jsx
+++ 41st_challenge/comps/src/components/Accordion.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 export function Accordion({ items }) {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -10,9 +11,16 @@ export function Accordion({ items }) {
   const renderedItems = items.map((item, index) => {
     const isActive = index === activeIndex;
 
+    const icon = <span>
+      {isActive ? <GoChevronDown /> : <GoChevronLeft />}
+    </span>
+
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {item.label}
+          {icon}
+        </div>
         {isActive && <div>{item.content}</div>}
       </div>
     );
