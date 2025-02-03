c436ec696ba038dc032bbf13372a0d30cceee1be
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-03 22:58:16 +0100

    Dropdown component start

--- 41st_challenge/comps/src/App.jsx
+++ 41st_challenge/comps/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
+import { Dropdown } from "./components/Dropdown";
 
+function App() {
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" }
+  ]
+
+  return (
+    <div className="App">
+      <Dropdown options={options} />
+    </div>
+  );
 }
 
 export default App
---
+++ 41st_challenge/comps/src/components/Dropdown.jsx
@@ -0,0 +1,5 @@
+export function Dropdown({ options, value, onChange }) {
+  return (
+    <div>Dropdown</div>
+  )
+}
