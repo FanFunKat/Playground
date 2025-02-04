70152453d4f70567deb6c7aff66b5d94b9056eac
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-04 23:14:25 +0100

    isOpen state and handleClick

--- 41st_challenge/comps/src/components/Dropdown.jsx
+++ 41st_challenge/comps/src/components/Dropdown.jsx
@@ -1,5 +1,28 @@
-export function Dropdown({ options, value, onChange }) {
+import { useState } from 'react';
+
+export function Dropdown({ options }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  }
+
+  const handelOptionClick = (option) => {
+    setIsOpen(false)
+  }
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handelOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    )
+  });
+
   return (
-    <div>Dropdown</div>
-  )
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
