6f7b42d40c3f93d7a288f7339386ec8694c005c8
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-07 22:19:36 +0100

    icon

--- 41st_challenge/comps/src/components/Dropdown.jsx
+++ 41st_challenge/comps/src/components/Dropdown.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { GoChevronDown } from "react-icons/go";
+
 
 export function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -20,15 +22,11 @@ export function Dropdown({ options, value, onChange }) {
     )
   });
 
-  // let content = 'Select...';
-  // if (selection) {
-  //   content = selection.label;
-  // }
-
   return (
     <div className='w-48 relative'>
       <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full' onClick={handleClick}>
         {value?.label || 'Select...'}
+        <GoChevronDown className='text-lg' />
       </div>
       {isOpen && <div className='absolute top-full rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
     </div>
