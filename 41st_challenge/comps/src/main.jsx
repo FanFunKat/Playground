7092380b05e09d85bb07045671c05ab53a5430ac
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-09 22:27:05 +0100

    created context

--- 41st_challenge/comps/src/main.jsx
+++ 41st_challenge/comps/src/main.jsx
@@ -2,9 +2,12 @@ import './index.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { NavigationProvider } from './context/navigation.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <NavigationProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </NavigationProvider>
 )
