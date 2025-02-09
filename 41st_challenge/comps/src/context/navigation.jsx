0873490912da1648d9f56f5202dbffffdb2ce772
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-09 22:52:15 +0100

    NavigationProvider & NavigationContext

--- 41st_challenge/comps/src/context/navigation.jsx
+++ 41st_challenge/comps/src/context/navigation.jsx
@@ -15,8 +15,13 @@ export function NavigationProvider({ children }) {
     };
   }, []);
 
+  const navigate = (to) => {
+    window.history.pushState({}, "", to);
+    setCurrentPath(to);
+  };
+
   return (
-    <NavigationContext.Provider value={{}}>
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
       {children}
     </NavigationContext.Provider>
   );
