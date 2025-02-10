ac6ab833820e54d8b3ae7bf7cb0e1e409414b923
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-10 22:56:17 +0100

    Route component

--- 41st_challenge/comps/src/App.jsx
+++ 41st_challenge/comps/src/App.jsx
@@ -1,12 +1,19 @@
 import { Link } from "./components/Link";
+import { Route } from "./components/Route";
+import { AccordionPage } from "./pages/AccordionPage";
+import { DropdownPage } from "./pages/DropdownPage";
 
 function App() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/accordion">About</Link>
-      <Link to="/dropdown">Contact</Link>
-      <Link to="/button">Products</Link>
+      <Link to="/accordion">Accordion</Link>
+      <Link to="/dropdown">Dropdown</Link>
+      <Route path="/accordion">
+        <AccordionPage />
+      </Route>
+      <Route path="/dropdown">
+        <DropdownPage />
+      </Route>
     </>
   )
 }
