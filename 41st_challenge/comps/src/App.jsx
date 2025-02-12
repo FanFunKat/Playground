cb1516e3c98ada55b1e144b2936d4cc447041110
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-12 23:32:10 +0100

    sidebar component

--- 41st_challenge/comps/src/App.jsx
+++ 41st_challenge/comps/src/App.jsx
@@ -1,20 +1,25 @@
-import { Link } from "./components/Link";
 import { Route } from "./components/Route";
+import { Sidebar } from "./components/Sidebar";
 import { AccordionPage } from "./pages/AccordionPage";
 import { DropdownPage } from "./pages/DropdownPage";
+import { ButtonPage } from "./pages/ButtonPage";
 
 function App() {
   return (
-    <>
-      <Link to="/accordion">Accordion</Link>
-      <Link to="/dropdown">Dropdown</Link>
-      <Route path="/accordion">
-        <AccordionPage />
-      </Route>
-      <Route path="/dropdown">
-        <DropdownPage />
-      </Route>
-    </>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
   )
 }
 
