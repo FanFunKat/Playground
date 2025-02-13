21ebfb73974b72c7775e81d8b90e211522e83071
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-13 22:47:27 +0100

    show close Modal

--- 41st_challenge/comps/src/pages/MadalPage.jsx
+++ 41st_challenge/comps/src/pages/MadalPage.jsx
@@ -1,9 +1,26 @@
 import { Modal } from "../components/Modal";
+import { Button } from "../components/Button";
+import { useState } from "react";
 
 export function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary >Open Modal</Button>
+      {showModal && <Modal onClose={handleClose} />}
     </div>
   );
 }
