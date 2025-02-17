6b278a685be55a1475a71f873cddaa84182cad30
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-17 23:21:09 +0100

    position: fixed

--- 41st_challenge/comps/src/components/Modal.jsx
+++ 41st_challenge/comps/src/components/Modal.jsx
@@ -11,9 +11,8 @@ export function Modal({ onClose, children, actionBar }) {
 
   return ReactDOM.createPortal(
     <div>
-      <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-      <div className="absolute inset-40 p-10 bg-white m-50
-">
+      <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+      <div className="fixed inset-40 p-10 bg-white rounded-lg shadow-lg">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">
