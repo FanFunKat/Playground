265b33850a8eedf4c8b39fd85eed41b28a3768f6
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-17 23:03:48 +0100

    customizing the modal

--- 41st_challenge/comps/src/components/Modal.jsx
+++ 41st_challenge/comps/src/components/Modal.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom';
 
-export function Modal({ onClose }) {
+export function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">
-        Modal
+        {children}
+        {actionBar}
       </div>
     </div>,
     document.querySelector('.modal-container')
