5b42f7549a16ed1b307c8a9475b1777685f7b7c5
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-02-28 16:44:16 +0100

    onSubmit

--- 41st_challenge/comps/src/pages/CounterPage.jsx
+++ 41st_challenge/comps/src/pages/CounterPage.jsx
@@ -23,6 +23,12 @@ export function CounterPage({ initialCount }) {
     setVAlueToAdd(value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setVAlueToAdd(0);
+  };
+
   return (
     <Panel className='m-3'>
       <h1 className='text-lg'>Count is {count}</h1>
@@ -35,7 +41,7 @@ export function CounterPage({ initialCount }) {
         </Button>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input className='p-1 m-3 bg-gray-50 border border-gray-300' type='number' value={valueToAdd || ""} onChange={handleChange} />
         <Button>Add it!</Button>
