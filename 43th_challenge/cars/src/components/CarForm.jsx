37df7faa1dc3c539c6d850f862297e60eb447261
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-03-19 22:46:39 +0100

    implement CarForm component with name input and Redux integration for state management

--- 43th_challenge/cars/src/components/CarForm.jsx
+++ 43th_challenge/cars/src/components/CarForm.jsx
@@ -1,5 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 export function CarForm() {
+  const dispatch = useDispatch();
+  const name = useSelector((state) => {
+    return state.form.name || "";
+  });
+
+  const handleNameChange = (e) => {
+    dispatch(changeName(e.target.value));
+  };
+
   return (
-    <div>car form </div>
+    <div className="car-form">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange} />
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
