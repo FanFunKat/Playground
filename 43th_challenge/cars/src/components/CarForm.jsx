414d8cbf9ded5fa352508b2d75c92c276115da68
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-03-20 17:52:04 +0100

    add cost input field to CarForm component with Redux integration for state management

--- 43th_challenge/cars/src/components/CarForm.jsx
+++ 43th_challenge/cars/src/components/CarForm.jsx
@@ -1,15 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName } from "../store";
+import { changeName, changeCost } from "../store";
 
 export function CarForm() {
   const dispatch = useDispatch();
   const name = useSelector((state) => {
     return state.form.name || "";
   });
+  const cost = useSelector((state) => {
+    return state.form.cost || 0;
+  });
 
   const handleNameChange = (e) => {
     dispatch(changeName(e.target.value));
   };
+
+  const handleCostChange = (e) => {
+    const carCost = parseInt(e.target.value) || 0;
+    dispatch(changeCost(carCost));
+  }
 
   return (
     <div className="car-form">
@@ -22,6 +30,14 @@ export function CarForm() {
               className="input is-expanded"
               value={name}
               onChange={handleNameChange} />
+          </div>
+          <div className="field">
+            <label className="label">Cost</label>
+            <input
+              className="input is-expanded"
+              value={cost || ""}
+              onChange={handleCostChange}
+              type="number" />
           </div>
         </div>
       </form>
