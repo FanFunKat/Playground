b7ec223685899260f0a20ac72688d6d24d4f6a69
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-03-21 22:02:33 +0100

    add submit functionality to CarForm component with addCar action dispatch

--- 43th_challenge/cars/src/components/CarForm.jsx
+++ 43th_challenge/cars/src/components/CarForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 export function CarForm() {
   const dispatch = useDispatch();
@@ -17,12 +17,17 @@ export function CarForm() {
   const handleCostChange = (e) => {
     const carCost = parseInt(e.target.value) || 0;
     dispatch(changeCost(carCost));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addCar({ name, cost }));
   }
 
   return (
     <div className="car-form">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -39,6 +44,9 @@ export function CarForm() {
               onChange={handleCostChange}
               type="number" />
           </div>
+        </div>
+        <div className="field">
+          <button className="button is-link" type="submit">Submit</button>
         </div>
       </form>
     </div>
