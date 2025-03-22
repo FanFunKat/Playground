bd799b328140c66dfd6880ca81189f96b35efdfb
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-03-22 21:51:36 +0100

    implement CarList component and update state structure in carsSlice

--- 43th_challenge/cars/src/components/CarList.jsx
+++ 43th_challenge/cars/src/components/CarList.jsx
@@ -1,7 +1,28 @@
+import { useSelector } from 'react-redux';
+
 export function CarList() {
+  const cars = useSelector(state => state.cars.data);
+  const handleCarDelete = (car) => {
+    console.log('Delete car:', car);
+  }
+  const renderedCars = cars.map(car => {
+    return (
+      <div key={car.id} className='panel'>
+        <p> {car.name} - ${car.cost}</p>
+        <button
+          className='button is-danger'
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  })
+
   return (
-    <div>
-      <h1>Car List</h1>
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
     </div>
   )
 }
