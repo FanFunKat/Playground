3f8be89f11058442b8ef6831643e83a07fbab55c
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-03-27 22:30:18 +0100

    add CarValue component to calculate and display total cost of filtered cars

--- 43th_challenge/cars/src/components/CarValue.jsx
+++ 43th_challenge/cars/src/components/CarValue.jsx
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+
 export function CarValue() {
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    let cost = 0;
+    filteredCars.forEach((car) => {
+      cost += car.cost;
+    });
+
+    return cost;
+  });
+
   return (
-    <div>
-      Car value
-    </div>
+    <div div className="car-value" >
+      Total cost: ${totalCost}
+    </div >
   );
 }
