e360362b6b976fdebd4a223ae3af67408d9bfde2
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-04-04 14:09:54 -0300

    Fetch users on component mount in UsersList component

--- 44th_challenge/media/src/components/UsersList.jsx
+++ 44th_challenge/media/src/components/UsersList.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
 
 export function UsersList() {
-  return (
-    <div>UsersList</div>
-  )
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  return 'Users List';
 }
