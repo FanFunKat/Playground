4d4f5213f9cedf0bbca030481b11362799b27e26
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-04-06 14:58:53 -0300

    Implement loading and error handling in UsersList component

--- 44th_challenge/media/src/components/UsersList.jsx
+++ 44th_challenge/media/src/components/UsersList.jsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { use, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 export function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return 'Users List';
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return <div>{data.length}</div>
+
+  // return 'Users List';
 }
