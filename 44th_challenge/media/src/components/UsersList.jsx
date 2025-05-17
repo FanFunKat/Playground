5f06ea5b436852c0d07e9b9a458799c416f9594c
Katarzyna Russek <katarzyna.russek@gmail.com>
2025-05-17 14:49:23 +0200

    Enhance UsersList component to manage loading state and handle errors during user fetching

--- 44th_challenge/media/src/components/UsersList.jsx
+++ 44th_challenge/media/src/components/UsersList.jsx
@@ -1,26 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import { Sceleton } from "./Sceleton";
 
 export function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => state.users);
+  const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .then(() => {
+        setIsLoadingUsers(false);
+      })
+      .catch((err) => {
+        setLoadingUsersError(err);
+        setIsLoadingUsers(false);
+      });
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   }
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Sceleton times={6} className='h-10 w-full' />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
